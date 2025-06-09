9102efea8251fd8c0606c5d7d4fc48d6b9debe21
Satvikpoojar <158794863+Satvikpoojar@users.noreply.github.com>
2025-06-09 16:06:02 +0530

    Add files via upload

---
+++ src/main/java/com/prog1/maven/prog1_example/com/App.java
@@ -0,0 +1,13 @@
+package com.prog1.maven.prog1_example.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
